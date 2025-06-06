8b172fdd0a66d636a2e15a60624e63e9e08d1879
serrabruna <brunaibserra@hotmail.com>
2025-06-05 22:09:27 -0300

    Validação do CPF

--- src/model/Usuario.ts
+++ src/model/Usuario.ts
@@ -8,6 +8,10 @@ export class Usuario{
     diaSuspensao: number;
     
     constructor(cpf: string, nome: string, email: string, categoria: string, curso: string){
+        if(!Usuario.validarCPF(cpf)){
+            throw new Error("CPF inválido!");
+        }
+        
         this.cpf = cpf;
         this.nome = nome;
         this.email = nome;
@@ -15,5 +19,37 @@ export class Usuario{
         this.curso = curso;
         this.status = 'ativo';
         this.diaSuspensao = 0;
+    };
+
+    static verificarSequenciaRepetida(cpf:string): boolean {
+        const primeiroDigito = cpf[0];
+        for(let i = 0; i < cpf.length; i++){
+            if(cpf[i] == primeiroDigito) return false;
+        }
+        return true;
+        
+    }
+
+    static validarCPF(cpf: string): boolean{
+        if(!cpf || cpf.length !== 11 || Usuario.verificarSequenciaRepetida(cpf)) return false;
+
+        const calcularDigito = (cpf: string, fator: number) => {
+            let total = 0;
+            for(let i = 0; i < fator - 1; i++){
+                total += parseInt(cpf[i]) * (fator - i);
+            }
+            const resto = total % 11;
+            if(resto < 2){
+                return 0; 
+            }
+            else{
+                return 11 - resto;
+            }
+        }
+
+            const digito1: number = calcularDigito(cpf, 10);
+            const digito2: number = calcularDigito(cpf, 11);
+
+            return digito1 === parseInt(cpf[9]) && digito2 === parseInt(cpf[10]);
     }
 }
